833c6d7d539382ce09945c2d515ffa1d9db50f1b
vikkicgreen <vikkicgreen@gmail.com>
2023-05-25 20:38:01 -0400

    initial commit

---
+++ src/test/java/com/sap/loottable/SapLootTablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.loottable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapLootTablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
